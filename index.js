ebe2973d440577722ed7be8ab7f5fc49ed4c2b85
OMesk <omesk001@doc.gold.ac.uk>
2025-01-16 10:40:55 +0000

    Low Quality initial push

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+const port = 8000;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.locals.siteData = { siteName: "Log Reader" };
+
+const mainRoutes = require('./routes/main');
+app.use('/', mainRoutes);         
+
+app.listen(port, () => console.log(`Node app listening on port ${port}!`));
